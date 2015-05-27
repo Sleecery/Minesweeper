7645c05b9b433d0485ce8c360d8ea905fee01eb0
Sleecery <fkonecny2@gmail.com>
2015-05-27 15:57:22 +0200

    asdasdasd

--- Minesweeper/Program.cs
+++ Minesweeper/Program.cs
@@ -17,7 +17,7 @@ namespace Minesweeper
             Console.WriteLine("");
             Console.WriteLine("Filo je pan");
             //
-            Console.WriteLine("Rado je kral a to je vacsie ako pan!");
+          
         }
     }
 }
