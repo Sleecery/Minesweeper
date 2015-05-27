1f2abaab938005703244c2ca4f0fc42f07933644
Juraj <juraj.lumtzer@ness.com>
2015-05-27 14:21:45 +0200

    NazovZmeny

--- Minesweeper/Program.cs
+++ Minesweeper/Program.cs
@@ -10,6 +10,8 @@ namespace Minesweeper
     {
         static void Main(string[] args)
         {
+            Console.WriteLine("Hello World!");
+            Console.ReadLine();
         }
     }
 }
